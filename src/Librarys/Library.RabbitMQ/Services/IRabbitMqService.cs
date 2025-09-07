0b33dab29bb423d685d9e2122d6320d6f9a01783
Brian <>
2025-09-07 20:24:49 +0800

    update lib rabbitmq

--- src/Librarys/Library.RabbitMQ/Services/IRabbitMqService.cs
+++ src/Librarys/Library.RabbitMQ/Services/IRabbitMqService.cs
@@ -3,4 +3,5 @@ namespace Library.RabbitMQ.Services;
 public interface IRabbitMqService
 {
     Task PublishAsync(string exchange, string routingKey, string message);
+    Task RmqEventDispatcher(string queue, string message);
 }
